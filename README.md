91fc1253f160f880b69c27795f725bced73fddfd
pritesh511 <patelpritesh313@gmail.com>
2023-06-24 19:26:58 +0530

    desing setting menu

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 ## This is food ordering app
 
+## What is use in this app:
+
+1. react
+2. react-router-dom
+3. antd
+4. ant-icons
+5. formik
+6. react-redux
+7. redux-toolkit
+8. parcel-bundler
+
+## What is best in project
+
+1. parcel use which make our whole app in bundle together
+2. formik for login and signup user validation
+
 ## package.json
 
 -> This files contain all dependency version which require to project
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -9,6 +9,7 @@ import {
 } from "@ant-design/icons";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Button, Popover } from "antd";
 
 const Header = () => {
   const item_in_cart = useSelector((state) => state?.cartslice?.cart);
@@ -18,53 +19,92 @@ const Header = () => {
     (state) => state?.userslice?.currentLoginUser
   );
 
+  const content = (
+    <ul className="menu-wrapper">
+      <li className="menu-link-item">
+        <Link to="/" className="menu-link">
+          Profile
+        </Link>
+      </li>
+      <li className="menu-link-item">
+        <Link to="/" className="menu-link">
+          Orders
+        </Link>
+      </li>
+      <li className="menu-link-item">
+        <Link to="/" className="menu-link">
+          Setting
+        </Link>
+      </li>
+      <li className="menu-link-item">
+        <Link to="/" className="menu-link">
+          FAQ
+        </Link>
+      </li>
+      <li className="menu-link-item">
+        <Link to="/" className="menu-link">
+          Logout
+        </Link>
+      </li>
+    </ul>
+  );
+
   return (
-    <div className="header-wrapper">
-      <div className="container">
-        <div className="header">
-          <div className="nav-logo">
-            <Link to="/">
-              <img src={LOGO_URL} alt="app-logo" />
-            </Link>
-          </div>
-          <ul className="nav-list">
-            <li className="nav-item">
-              <Link to="/search">
-                <SearchOutlined />
-                <span className="nav-text">Search</span>
+    <>
+      <div className="header-wrapper">
+        <div className="container">
+          <div className="header">
+            <div className="nav-logo">
+              <Link to="/">
+                <img src={LOGO_URL} alt="app-logo" />
               </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/help">
-                <GlobalOutlined />
-                <span className="nav-text">Help</span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/cart">
-                <ShoppingCartOutlined />
-                <span className="nav-text">Cart</span>
-                {qty_in_cart !== 0 && (
-                  <span className="cart-item-badge">{qty_in_cart}</span>
-                )}
-              </Link>
-            </li>
-            {current_user_login ? (
+            </div>
+            <ul className="nav-list">
               <li className="nav-item">
-                <div className="login-user">{current_user_login?.name[0]}</div>
-              </li>
-            ) : (
-              <li className="nav-item">
-                <Link to="/login">
-                  <UserOutlined />
-                  <span className="nav-text">Sign in</span>
+                <Link to="/search">
+                  <SearchOutlined />
+                  <span className="nav-text">Search</span>
                 </Link>
               </li>
-            )}
-          </ul>
+              <li className="nav-item">
+                <Link to="/help">
+                  <GlobalOutlined />
+                  <span className="nav-text">Help</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/cart">
+                  <ShoppingCartOutlined />
+                  <span className="nav-text">Cart</span>
+                  {qty_in_cart !== 0 && (
+                    <span className="cart-item-badge">{qty_in_cart}</span>
+                  )}
+                </Link>
+              </li>
+              {current_user_login ? (
+                <li className="nav-item">
+                  <Popover
+                    className="login-user"
+                    placement="bottomRight"
+                    content={content}
+                    trigger="click"
+                  >
+                    <Button>{current_user_login?.name[0]}</Button>
+                  </Popover>
+                </li>
+              ) : (
+                <li className="nav-item">
+                  <Link to="/login">
+                    <UserOutlined />
+                    <span className="nav-text">Sign in</span>
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
