65d342c5f597f2d85ca84bbfd609564aee7da073
pritesh511 <94275226+pritesh511@users.noreply.github.com>
2023-12-29 15:53:18 +0530

    Update proxy url

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,7 +5,8 @@ export const BASE_IMG_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const RES_API_URL =
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
+  // "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
+  "https://api.allorigins.win/get?url=" + encodeURIComponent("https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING")
 
 export const RES_MENU_API =
   "https://thingproxy.freeboard.io/fetch" +
