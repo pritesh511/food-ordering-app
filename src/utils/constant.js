7140004980ca8b953d9aea2e9dc76bad57016d17
pritesh511 <94275226+pritesh511@users.noreply.github.com>
2023-12-29 15:46:06 +0530

    Update constant.js

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -5,13 +5,13 @@ export const BASE_IMG_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const RES_API_URL =
-  "https://proxy.cors.sh?" +
+  "https://thingproxy.freeboard.io/fetch" +
   encodeURIComponent(
     "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING"
   );
 
 export const RES_MENU_API =
-  "https://proxy.cors.sh?" +
+  "https://thingproxy.freeboard.io/fetch" +
   encodeURIComponent(
     "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001"
   );
